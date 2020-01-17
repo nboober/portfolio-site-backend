b8e398e3df261cf354b7eb9ff360fa40b71e5ad6
Nick Boober <nboober@terpmail.umd.edu>
2020-01-17 14:50:26 -0500

    successfully incorporated medium posts api

--- app/controllers/medium_controller.rb
+++ app/controllers/medium_controller.rb
@@ -1,7 +1,11 @@
 class MediumController < ApplicationController
 
-   def getPost
-        
+   def get_posts
+        response = RestClient.get(
+            'https://api.rss2json.com/v1/api.json?rss_url=https://medium.com/feed/@skinsfannick'
+        )
+
+        render json: response
     end
 
 end
